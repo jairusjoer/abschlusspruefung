5fe4d812c682c35de0506c2f7717d43ce91cf5fe
Jairus Joer・Macbook Pro <jairus.joer@neuwaerts.de>
2021-06-08 15:22:01 +0200

    Prepare final build

--- source/index.js
+++ source/index.js
@@ -1,43 +1,14 @@
-// MILESTONES SlIDER
-new Glide(".milestones-glide", {
-  type: "carousel",
-  gap: 24,
-}).mount();
-
-// COMMENTS SLIDER
-new Glide(".comments-glide", {
-  type: "carousel",
-  autoplay: 3000,
-  gap: 24,
-  peek: 216,
-  breakpoints: {
-    768: {
-      peek: 48,
-    },
-  },
-}).mount();
-
+// Open navigation on click on menu icon
 const nav = document.getElementsByTagName("nav")[0];
 const body = document.getElementsByTagName("body")[0];
 document.getElementById("toggleMenu").onclick = () => {
   nav.classList.toggle("open");
-  body.classList.toggle("noscroll");
 };
 
-/* FEATURE: DARK MODE
-const body = document.getElementsByTagName("body")[0];
-// Check available local storage
-if (localStorage.getItem("mode") == "dark") {
-  body.classList.toggle("dark");
+// Close navigation on any link click inside navigation
+const link = document.getElementsByClassName("navigation-menu-item");
+for (const item of link) {
+  item.onclick = () => {
+    nav.classList.toggle("open");
+  };
 }
-// Toggle state on click event
-document.getElementById("toggleState").onclick = () => {
-  // Check and update current state
-  if (localStorage.getItem("mode") == "dark") {
-    localStorage.setItem("mode", "light");
-  } else {
-    localStorage.setItem("mode", "dark");
-  }
-  body.classList.toggle("dark");
-};
-*/
